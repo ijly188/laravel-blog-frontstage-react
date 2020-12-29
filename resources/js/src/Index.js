746f333b42ed99474f8c901134d27eae577c7102
ava.chang <avachang.testritegroup@gmail.com>
2020-12-29 10:57:14 +0800

    create folder structure

--- resources/js/src/Index.js
+++ resources/js/src/Index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux'
 // import configureStore from './src/store/configureStore';
+import store from './store'
 
 // 後面可能可以加的就是polyfill，用來支援ie舊版本的東西
 
-import App from './src/App';
+import App from './App';
 
 // const store = configureStore();
 
@@ -13,11 +15,13 @@ import App from './src/App';
 // app.js檔案是laravel預設的不調整原有的東西去擴充，所以多做了一個 /app/index.js
 // 這邊只要確定redux有灌進去就好了
 render(
-  <BrowserRouter>
-    <Switch>
-      <App />
-    </Switch>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <App />
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('app')
 );
 
